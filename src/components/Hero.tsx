134e8eb7e9fd5a870f603f02ca2b0ef447ee4e7d
melissasenaalonso-rgb <melissa.senaalonso@gmail.com>
2025-12-30 18:49:16 -0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -94,7 +94,7 @@ const Hero = () => {
       {/* Animated waves decoration */}
       <div className="absolute bottom-0 left-0 right-0 h-32 overflow-hidden z-10">
         <svg 
-          viewBox="0 0 1770 120" 
+          viewBox="0 0 1550 120" 
           className="absolute bottom-0 w-full h-32 text-background animate-wave"
           preserveAspectRatio="none"
         >
