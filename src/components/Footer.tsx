c1b5f618ab55d1ebad246447f849084d52d9f7d2
El-sides <vitorsenasides@gmail.com>
2025-12-30 01:16:20 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,21 +3,6 @@ import logoAquazul from "@/assets/logo-aquazul.jpg";
 
 const Footer = () => {
   return (
-    {/* Marcas Parceiras */}
-<section class="marcas-parceiras">
-  <h2>Marcas Parceiras</h2>
-
-  <div class="conteudo-marcas">
-    <img src="img/sodrama.png" alt="Logo Sodrama">
-    <img src="img/nautilos.png" alt="Logo Naútilos">
-    <img src="img/genco.png" alt="Logo Genco">
-    <img src="img/hth.png" alt="Logo HTH">
-    <img src="img/rheem.png" alt="Logo Rheem">
-    <img src="img/sibrape.png" alt="Logo Sibrape">
-    <img src="img/tholz.png" alt="Logo Tholz">
-  </div>
-</section>
-      
     <footer className="bg-secondary text-secondary-foreground">
       <div className="container mx-auto px-4 py-16">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12">
